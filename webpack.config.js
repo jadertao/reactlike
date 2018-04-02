6c9eb8e655ddd70e6a6f0ac44b3eb352890283a4
jader_tao <jader_tao@qq.com>
2018-04-02 14:19:12 +0800

    init: typescript dev env

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: path.join(__dirname, '/app.ts'),
+  output: {
+    filename: 'app.js',
+    path: __dirname
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+};
