6913f9c8a838f9b2f4cdd616e597a1b35b0d8118
jader_tao <jader_tao@qq.com>
2018-07-03 09:14:54 +0800

    adjust: dev stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
+
+
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
-  entry: path.join(__dirname, '/src/index.ts'),
+  entry: path.join(__dirname, '/src/app.tsx'),
   output: {
     filename: 'app.js',
     path: __dirname,
@@ -11,13 +13,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
+        test: /\.ts(x?)$/,
         exclude: /node_modules/,
-      },
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
     ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js", ".jsx"]
   },
 };
