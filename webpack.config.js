0e1808928ff3408a10c42a83d36c0d132d1bf68d
jader_tao <jader_tao@qq.com>
2018-12-12 23:27:38 +0800

    dev: config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,27 @@
 const path = require('path');
 
-
-
 module.exports = {
   mode: "development",
-  devtool: "inline-source-map",
-  entry: path.join(__dirname, '/src/app.tsx'),
+  devtool: "cheap-module-eval-source-map",
+  entry: path.join(__dirname, '/example/app.tsx'),
   output: {
     filename: 'app.js',
     path: __dirname,
   },
   module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
+    rules: [{
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'ts-loader'
+      }]
+    }]
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js", ".jsx"]
   },
+  devServer: {
+    overlay: true,
+    stats: "minimal",
+  }
 };
