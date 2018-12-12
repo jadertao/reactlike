9154850360abf2f0c720d33f597cab8c8d9113dc
jader_tao <jader_tao@qq.com>
2018-12-12 23:59:59 +0800

    dev: change example

--- example/timer.tsx
+++ example/timer.tsx
@@ -1,9 +1,10 @@
 import { render, createElement } from "../src";
 
 export function timer(rootDom): number {
+  const a = 1
   function tick() {
     const time = new Date().toLocaleTimeString();
-    const clockElement = <h1>{time}</h1>;
+    const clockElement = <h1>{time}<div>{2 * a}</div></h1>;
     render(clockElement, rootDom);
   }
 
