390a9b9b2a190eb6078e56cc962717f734fc4e3a
jader_tao <jader_tao@qq.com>
2018-12-18 23:39:57 +0800

    dev: update example

---
+++ example/app-v2.tsx
@@ -0,0 +1,17 @@
+import { Component } from "../src/compoent";
+import { render, createElement } from "../src";
+
+export class App extends Component {
+  state = {
+    time: new Date()
+  }
+  render() {
+    setInterval(() => { this.setState({ time: new Date() }) }, 1000)
+    return (
+      <h2>{this.state.time}</h2>
+    )
+  }
+}
+
+const rootDom = document.getElementById('host');
+render(<App />, rootDom);
--- example/app.tsx
+++ example/app.tsx
@@ -1,17 +1,27 @@
 import { Component } from "../src/compoent";
 import { render, createElement } from "../src";
-
-export class App extends Component {
-  state = {
-    time: new Date()
-  }
+const data = ['long long ago,', 'there is a fish', 'swimming in the sky'];
+class App extends Component<{ data: Array<string> }> {
   render() {
-    setInterval(() => { this.setState({ time: new Date() }) }, 1000)
     return (
-      <h2>{this.state.time.toUTCString()}</h2>
-    )
+      <div><h1>full react</h1>
+        <ul>{this.props.data.map((v: string) => <Item name={v} />)}</ul>
+      </div>
+    );
   }
 }
-
-const rootDom = document.getElementById('host');
-render(<App />, rootDom);
+class Item extends Component<{ name: string }, { likes: number }> {
+  state = { likes: Math.ceil(Math.random() * 100) };
+  like = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  }
+  render() {
+    const { likes } = this.state;
+    return (
+      <li><button onClick={this.like}>{likes}<b>❤</b><span>{this.props.name}</span></button></li>
+    );
+  }
+}
+render(<App data={data} />, document.getElementById('host'));
